0a0fabb0e3a7a3bbee991f1f97e5bb72e579732b
Panu Aho <panu.aho@wapice.com>
2021-03-04 11:53:44 +0200

    implement util script for writing tags to mongoDB

--- utils/tags_to_file.py
+++ utils/tags_to_file.py
@@ -1,9 +1,33 @@
+import os
 import argparse
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from nlp.nlp import get_tags
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', default='../data/tags.csv')
+    parser.add_argument('--target', default='file')
+
     args = parser.parse_args()
     tags = get_tags()
-    tags.to_csv(args.path)
+
+    if args.target == 'file':
+        tags.to_csv(args.path)
+    
+    elif args.target == 'dev':
+        load_dotenv('../.env-dev')
+        uri = os.getenv('MONGODB_URI_DEV')
+        db_name = 'tavaralle-hinta'
+        client = MongoClient(uri)
+        db = client[db_name]
+        db.tags.insert_many(tags.to_dict(orient='records'))
+
+    elif args.target == 'prod':
+        load_dotenv('../.env')
+        uri = os.getenv('MONGODB_URI')
+        db_name = 'tavaralle-hinta'
+        client = MongoClient(uri)
+        db = client[db_name]
+        db.tags.insert_many(tags.to_dict(orient='records'))
